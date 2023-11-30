33372fa6f7bf2baa4f6042ca2be768bbfba002d6
FilipBorsodi <152484187+FilipBorsodi@users.noreply.github.com>
2023-11-30 11:33:12 +0100

    Update

--- test.cpp
+++ test.cpp
@@ -8,19 +8,10 @@
 
 int main(int argc, char *argv[]){			
 	std::vector<float> tester = {0,1,2,3,4,5,6,7,8,9};
-	int size = 1;
+	int size = 10;
 	int rank = 0;
-	std::vector<std::vector<float>> matrix;
 	std::vector<float> row;	
-
-	for(int x;x<size;x++){
-		for(int y;y<size;y++){
-			row.push_back(x+y);		
-		}
-		matrix.push_back(row);
-		row.clear();
-	}
-	const std::size_t Nx = tester.size();	
+	const std::size_t Nx = tester.size();
 
 	adios2::ADIOS adios;
 	adios2::IO bpIO = adios.DeclareIO("bpIO");
@@ -32,14 +23,20 @@ int main(int argc, char *argv[]){
 	adios2::Engine bpWriter = bpIO.Open(filename, adios2::Mode::Write);
 	
 	bpWriter.BeginStep();
-	
-	bpWriter.Put(bpTester,tester.data());
+
+	for(int x;x<size;x++){
+		for(int y;y<size;y++){
+			row.push_back(x+y);		
+		}
+		bpWriter.Put(bpTester,tester.data());
+		row.clear();
+	}
 	
 	bpWriter.EndStep();
 
 	bpWriter.Close();	
 	 
-	std::cout << "Finished";		
+	std::cout << "Finished" << /n;		
 	return 0;
 }
 
